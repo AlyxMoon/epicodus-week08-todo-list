f98225d130a475ca2168f0e12e8361c42335f19d
AlyxMoon <allisterkmoon@gmail.com>
2021-07-27 08:51:02 -0700

    update tests to use a database

--- ToDoList.Tests/ModelsTests/CategoryTests.cs
+++ ToDoList.Tests/ModelsTests/CategoryTests.cs
@@ -14,6 +14,11 @@ namespace ToDoList.Tests
       Category.ClearAll();
     }
 
+    public CategoryTests()
+    {
+      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
+    }
+
     [TestMethod]
     public void CategoryConstructor_CreatesInstanceOfCategory_Category()
     {
